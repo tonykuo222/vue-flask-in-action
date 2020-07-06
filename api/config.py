04404c432b24c1fc59ff8473f392c9ef0bcf2584
yangyunf <yangyunfei07@gmail.com>
2020-07-06 10:06:00 +0800

    init

--- api/config.py
+++ api/config.py
@@ -13,7 +13,8 @@ log_dir = os.path.join(basedir, os.getenv('LOG_DIR', 'logs'))
 class Config(object):
     SECRET_KEY = 'talkpython'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///books.db'
-    #SQLALCHEMY_DATABASE_URI = "mysql+pymysql://yangyunfei:G6xmo2dQ5XtC7K78@47.105.80.104:6612/mp_chongai"
+    #mysql数据库连接方法
+    #SQLALCHEMY_DATABASE_URI = "mysql+pymysql://username:password@ip:port/db_name"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     LOG_LEVEL = logging.INFO
